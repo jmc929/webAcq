e17d5df6ea55ecde527fec370e1af156916338fb
jmc929 <150258753+jmc929@users.noreply.github.com>
2024-11-05 20:14:28 -0500

    fix image

--- js todo/index.js
+++ js todo/index.js
@@ -1,22 +1,21 @@
 const images = [
-  // Asegúrate de que la imagen estática no esté en este array
   'images/tractor y mangueras galeria.jpeg',
-  'images/riegos galeria.jpg',
+  'images/riegos  galeria.jpg',
   'images/panel galeria.jpg',
   'images/Manguera y campo galeria.jpeg',
   'images/IMPERMEABILIZACION DE TANQUE 3 galeria.jpeg'
 ];
 let currentIndex = 0;
-const intervalTime = 10000; // Tiempo en milisegundos (3000 ms = 3 segundos)
+const intervalTime = 10000; // Tiempo en milisegundos (10000 ms = 10 segundos)
 let autoShowInterval; // Variable para almacenar el intervalo de la presentación automática
 
 function showImage(index) {
   const galleryImage = document.getElementById('gallery-image');
-  galleryImage.style.opacity = 0;
+  galleryImage.style.opacity = 0; // Hacer la imagen invisible para el cambio
   setTimeout(() => {
-      galleryImage.src = images[index];
-      galleryImage.style.opacity = 1;
-  }, 500);
+      galleryImage.src = images[index]; // Cambiar la fuente de la imagen
+      galleryImage.style.opacity = 1; // Hacer la imagen visible después de cambiarla
+  }, 500); // Este retraso debe coincidir con la duración del efecto de desvanecimiento
 }
 
 function prevImage() {
@@ -42,5 +41,9 @@ function resetAutoShow() {
   startAutoShow(); // Iniciar un nuevo intervalo
 }
 
-// Iniciar la presentación automática cuando se carga la página
-window.onload = startAutoShow;
+// Asegurarse de que la primera imagen se muestre correctamente al cargar la página
+window.onload = function() {
+  showImage(currentIndex); // Mostrar la primera imagen
+  startAutoShow(); // Iniciar la presentación automática
+};
+
